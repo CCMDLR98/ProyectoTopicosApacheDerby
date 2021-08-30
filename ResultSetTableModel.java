cc5735327db9ae965f6c9fe06f262a36578dcd37
CCMDLR <carlitos_1098_@hotmail.com>
2021-08-30 13:15:19 -0600

    comentarios

--- ResultSetTableModel.java
+++ ResultSetTableModel.java
@@ -55,7 +55,7 @@ public class ResultSetTableModel extends AbstractTableModel
    {   
 
       if ( !conectadoABaseDatos ) 
-         throw new IllegalStateException( "No hay conexión a la base de datos" );
+         throw new IllegalStateException( "No hay conexiï¿½n a la base de datos" );
 
       try 
       {
@@ -138,28 +138,10 @@ public class ResultSetTableModel extends AbstractTableModel
          {                                            
             excepcionSql.printStackTrace();           
          }                          
-         finally  // actualiza el estado de la conexión a la base de datos
+         finally  // actualiza el estado de la conexiï¿½n a la base de datos
          {                                            
             conectadoABaseDatos = false;              
          }                          
       }
    }        
 }
-
-
-
-
-/**************************************************************************
- * (C) Copyright 1992-2007 por Deitel & Associates, Inc. y                *
- * Pearson Education, Inc. Todos los derechos reservados.                 *
- *                                                                        *
- * RENUNCIA: Los autores y el editor de este libro han realizado su mejor *
- * esfuerzo para preparar este libro. Esto incluye el desarrollo, la      *
- * investigación y prueba de las teorías y programas para determinar su   *
- * efectividad. Los autores y el editor no hacen ninguna garantía de      *
- * ningún tipo, expresa o implícita, en relación con estos programas o    *
- * con la documentación contenida en estos libros. Los autores y el       *
- * editor no serán responsables en ningún caso por los daños consecuentes *
- * en conexión con, o que surjan de, el suministro, desempeño o uso de    *
- * estos programas.                                                       *
- *************************************************************************/
